54f12c90dccf9ae9cd0ee762a51f11c3b1e82a20
many <emanmomot@gmail.com>
2017-12-14 14:18:30 +0100

    shake cabinet

--- Billy Bouncer/Assets/Scripts/ShakeCabinet.cs
+++ Billy Bouncer/Assets/Scripts/ShakeCabinet.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class ShakeCabinet : MonoBehaviour {
 
 	public int dir;
+	public Rigidbody cabinet;
+	public Transform forcePos;
+
+	private Vector3 baseForce;
 
 	// Use this for initialization
 	void Start () {
-		
+		baseForce = new Vector3 (dir * cabinet.mass / Time.deltaTime * .02f, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Vector3 force = Vector3.Dot (baseForce, collision.relativeVelocity) * new Vector3 (dir, 0, 0);
+		cabinet.AddForceAtPosition (force, forcePos.position);
 	}
 }
