578e2df3c330fc104cf51fe8908ec471b4c19431
Tyler Cauthen <tcauthe1@gmail.com>
2017-12-19 17:05:19 -0500

    destructable bar

---
+++ Billy Bouncer/Assets/breakIt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakIt : MonoBehaviour {
+	public GameObject bottom;
+	public AudioSource aSource;
+	public AudioClip[] sfxBreak;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag == "Floor")
+		{
+			int rando = Random.Range(0,3);
+			bottom.transform.parent = null;
+			bottom.AddComponent<BoxCollider>();
+			bottom.AddComponent<Rigidbody>();
+			aSource.PlayOneShot(sfxBreak[rando],1f);
+		}
+	}
+}
